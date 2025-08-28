88c4cfcd05a76401125faf9b03494583474d59c8
Surekha Gadkari <2000085665@hexaware.com>
2025-08-28 12:13:05 +0530

    Added Car Details Page along with routes

--- carinsuranceclaim/src/components/App.js
+++ carinsuranceclaim/src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import CarDetails from "./CarDetails";
+
 const App =()=> {
   return (
-    <div className="App"> 
-    Routes 
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/details">CarDetails</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<CarDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
