f84fa54e0971323d739aaf51860f0121dd65f91f
Surekha Gadkari <2000085665@hexaware.com>
2025-09-03 18:29:28 +0530

    Added pages

--- carinsuranceclaims/src/components/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dashboard  = ()=> {
-    return(<>Dashboard Page</>)
-};
-
-export default Dashboard;
--- carinsuranceclaims/src/components/PolicyDetails.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PolicyDetails  = ()=> {
-    return(<>PolicyDetails Page</>)
-};
-
-export default PolicyDetails;
