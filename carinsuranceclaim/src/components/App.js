c7de99ac6b7ddc481d39920871691284df6527c1
Surekha Gadkari <2000085665@hexaware.com>
2025-09-03 18:38:48 +0530

    Routes in progress

--- carinsuranceclaim/src/components/App.js
+++ carinsuranceclaim/src/components/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import CarDetails from "./CarDetails";
+import Dashboard from "./Dashboard";
+import Offers from "./Offers";
+import AcceptOffer from "./AccepteOffer";
+import RejectOffer from "./RejestOffer";
+import ClaimSubmission from "./ClaimSubmission";
 
 const App =()=> {
   return (
@@ -12,6 +17,9 @@ const App =()=> {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details" element={<CarDetails />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/submission" element={<ClaimSubmission />} />
       </Routes>
     </BrowserRouter>
   );
